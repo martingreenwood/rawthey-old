77f3cffa5d9a7bd3a89edbc012a3a93d58784e3a
Martin Greenwood <martin@pixelpudu.com>
2016-11-10 08:58:09 +0000

    removed typekit

--- functions.php
+++ functions.php
@@ -119,16 +119,6 @@ add_action( 'wp_enqueue_scripts', 'rawthey_scripts' );
  */
 function rawthey_head_scripts() {
 ?>
-<script>
-  (function(d) {
-    var config = {
-      kitId: 'nue2tim',
-      scriptTimeout: 3000,
-      async: true
-    },
-    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-  })(document);
-</script>
 <!--Start of HappyFox Live Chat Script-->
 <script>
  window.HFCHAT_CONFIG = {
